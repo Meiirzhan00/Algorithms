6d436e973aaec97403f6e91c8a6fada5a5ed62e6
Meiirzhan <54979320+Meiirzhan00@users.noreply.github.com>
2021-03-05 18:34:14 +0600

    Create Binary_search.py

---
+++ Binary_search.py
@@ -0,0 +1,20 @@
+from typing import List
+class Algorithm:
+  def Binary_search(self, list: List[int],item) -> int:
+    low=0
+    high=len(list)-1
+    while low<=high:
+      mid=(low+high)//2
+      guess=list[mid]
+      if guess==item:
+        return mid
+      elif guess>item:
+        high=mid-1
+      else:
+        low=mid+1
+    
+    return None
+
+my_list=[1,3,5,7,9]
+p=Algorithm()
+print(p.Binary_search(my_list,9))
